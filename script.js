18d3862f7756c8ee477dadfa17b02b7d0bd11e3c
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-09-30 09:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const API_KEY = 'dbe88f19b79f55ab6a5298ce9e8f135b'; 
+const API_KEY = 'dbe88f19b79f55ab6a5298ce9e8f135b';
 
 const button = document.querySelector('button');
 const weatherDiv = document.getElementById('weatherData');
 
-function getCurrentWeather(){
-	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
+function getCurrentWeather() {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
 
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            weatherDiv.innerHTML = `<h1>Weather API</h1>
-                                    <p>Current weather in London: ${data.weather[0].main}</p>
-                                    <button>Get Current Weather</button>`;
-        })
-        .catch(error => {
-            weatherDiv.innerHTML = `<h1>Weather API</h1>
-                                    <p>Error fetching weather data: ${error.message}</p>
-                                    <button>Get Current Weather</button>`;
-        });
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      weatherDiv.innerHTML = `<h1>Weather API</h1>
+                              <p>Current weather in London: ${data.weather[0].main}</p>
+                              <button>Get Current Weather</button>`;
+    })
+    .catch(error => {
+      weatherDiv.innerHTML = `<h1>Weather API</h1>
+                              <p>Error fetching weather data: ${error.message}</p>
+                              <button>Get Current Weather</button>`;
+    });
 }
 
 button.addEventListener('click', getCurrentWeather);
